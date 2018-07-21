46bd8f5b2f7e0f47898984a639c012dcb39062ac
James Harrington <JRileyHarrington@gmail.com>
2018-07-21 18:28:23 -0400

    absolute path for prod and ignore DS Store

--- src/js/loader.js
+++ src/js/loader.js
@@ -4,10 +4,10 @@ export default (cb, handleProgress)=>{
     let loader = PIXI.loader;
     //fire particles
     for(let i = 1; i <= 6; i++){
-        loader.add(`fire${i}`, `../assets/particles/fire/F${i}.png`)
+        loader.add(`fire${i}`, `public/assets/particles/fire/F${i}.png`)
     }
     loader
-        .add('gear', '../assets/icons/gear.png')
+        .add('gear', 'public/assets/icons/gear.png')
         .on("progress", handleProgress)
         .load(cb);
 }
