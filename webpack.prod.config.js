e9107f508fa654014003111c0f1c6d5330fa3a69
James Harrington <JRileyHarrington@gmail.com>
2018-07-21 03:28:59 -0400

    revert minification because breakage

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: "./src/js/index.js",
   output: {
     path: path.resolve(__dirname, "public"),
